9726427d350d3da76a665492e11a84d8e9c97cd1
theone8907 <franklincampos8907@gmail.com>
2024-08-15 00:59:53 -0400

    update link

--- src/app/excursions/[slug]/page.js
+++ src/app/excursions/[slug]/page.js
@@ -12,43 +12,30 @@ import useOpinions from "@/hook/useOpinions";
 
 function DetailPage({ params }) {
   const { language } = useGlobalContext();
-  const [excursion, setExcursion] = useState(null);
-  const { opinions, isLoading, setLoading } = useOpinions()
 
-  const getIdExcursion = (params) => {
-    const excursionDataGet = excursionsData
-    const filtersBySlug = excursionDataGet.excursions_es.find(obj => obj.slug = params.slug)
-    const id = filtersBySlug ? filtersBySlug.id : null;
-    return id
-  }
-  const idExcursion = getIdExcursion(params)
-
+  const { isLoading, opinions, setLoading } = useOpinions()
   useEffect(() => {
-
-    const dataExcursions = language === 'es' ? excursionsData.excursions_es : excursionsData.excursions_en
-    const objetExcursions = dataExcursions[idExcursion]
-
-    setExcursion(objetExcursions)
 
   }, []);
 
+  const sortData = language === "es"
+    ? excursionsData.excursions_es.sort((a, b) => a.precio_final - b.precio_final)
+    : excursionsData.excursions_en.sort((a, b) => a.precio_final - b.precio_final);
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  const endData = sortData.find(obj => obj.slug === params.slug);
+  const sortDataId = endData.id
+  if (!endData) redirect('/');
 
-  if (!excursion) {
-    return <p>Excursion not found</p>;
-  }
-
-  const result = opinions ? calcularPromedioValoraciones(opinions, excursion.id) : 0;
+  const result = opinions ? calcularPromedioValoraciones(opinions, sortDataId) : 0;
 
   return (
     <>
-      {opinions ? <Details datos={excursion} resultado={result} /> : <Loading />}
-      {excursion.places.map((i) => (
-        <Places key={crypto.randomUUID()} datos={i} />
+
+      {opinions ? <Details datos={endData} resultado={result} /> : <Loading />}
+      {endData.places.map((i, index) => (
+        <Places key={index} datos={i} />
       ))}
+
     </>
   );
 }
