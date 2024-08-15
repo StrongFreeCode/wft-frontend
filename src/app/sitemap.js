2cd7e9bee81a164296496616f9a8e31348936545
theone8907 <franklincampos8907@gmail.com>
2024-08-14 21:24:38 -0400

    update roboto and sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,53 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://wonderfulltime.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://wonderfulltime.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://wonderfulltime.com/excursions',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://wonderfulltime.com/excursions/the-havana-tour.com/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://wonderfulltime.com/excursions/zapata-swamp-excursion',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://wonderfulltime.com/excursions/vinales-valley-excursion',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://wonderfulltime.com/excursions/varadero-excursion',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://wonderfulltime.com/excursions/trinidad-excursion',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+
+  ]
+}
