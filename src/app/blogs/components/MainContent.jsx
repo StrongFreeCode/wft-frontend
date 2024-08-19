929d76c9739698f7b522b4bd1bf8a2df7074eb83
theone8907 <franklincampos8907@gmail.com>
2024-08-18 22:16:29 -0400

    Todo OK refacto OK

--- src/app/blogs/components/MainContent.jsx
+++ src/app/blogs/components/MainContent.jsx
@@ -2,7 +2,7 @@
 import PostsSection from './PostsSection';
 import SidebarSection from './SidebarSection';
 
-export default function MainContent({ articles, tagColors, selectedTags, onTagSelect, categories, onSubCategorySelect }) {
+export default function MainContent({ articles, tagColors, selectedTags, onTagSelect, categories, onSubCategorySelect, tags }) {
     return (
         <div className="container mx-auto flex flex-wrap py-6">
             <PostsSection articles={articles} tagColors={tagColors} />
